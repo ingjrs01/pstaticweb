d3760719361cdef7b66ee2003a59a679c2e43847
ingjrs01 <juanriveiro@gmail.com>
2021-01-13 11:25:36 +0100

    Se está añadiendo la portada en md

---
+++ content/home.md
@@ -0,0 +1,1 @@
+# Por que no te guardas
---
+++ pages/portada.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>Hola desde arriba</h1>
+      <p>Aqui el contenido</p>
+      <p><NLink to="/" class="button--grey">Volver</NLink></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      name: process.server ? "server" : "client",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
